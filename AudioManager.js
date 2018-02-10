d35279045d1e9baadd870efc5cee9bd06d9fd870
M D Fewkes <mdfewkes@gmail.com>
2018-02-09 23:46:30 -0700

    Refigured for Github Pages
    Abandoned archiving

--- AudioManager.js
+++ AudioManager.js
@@ -2,11 +2,11 @@ setFormat();
 isMuted = false;
 
 //set sound clips and music tracks here
-var track1 = new musicTrackLooping("../Audio/CotijaMenu120");
-var track2 = new musicTrackLooping("../Audio/M_Pt3_B");
+var track1 = new musicTrackLooping("Audio/CotijaMenu120");
+var track2 = new musicTrackLooping("Audio/M_Pt3_B");
 var backgroundMusic = new musicContainer(track1);
 
-var clip1 = new sfxClipDouble("../Audio/Death");
+var clip1 = new sfxClipDouble("Audio/Death");
 
 track1.setTrackName("track1");
 track2.setTrackName("track2");
