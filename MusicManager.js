f6d5f035c7a9485851a525086bad1f7319d94aa9
M D Fewkes <mdfewkes@gmail.com>
2018-02-10 15:35:09 -0700

    Filled out features, particularly LOOP

--- MusicManager.js
+++ MusicManager.js
@@ -1,6 +1,6 @@
 const REMOVE = 0; // Arrayformat [REMOVE]
 const FADE = 1; // Arrayformat [FADE, track, starttime, endtime, startvolume, endvolume]
-const LOOP = 2; // Arrayformat [LOOP, track]
+const LOOP = 2; // Arrayformat [LOOP, track, endtime]
 
 var date = new Date();
 
@@ -14,23 +14,61 @@ function musicEventManager() {
 		cleanupList();
 	}
 
-	this.addFadeEvent = function(track, duration, startVol, endVol) {
+	this.addFadeEvent = function(track, duration, endVol) {
+	var check = checkListFor(FADE, track);
+	var endTime = duration * 1000 + now;
+	var startvolume = track.getVolume();
 
+		if (check == "none") {
+			eventList.push([FADE, track, now, endTime, startVolume, endVol]);
+		} else {
+			eventList[check]] = [FADE, track, now, endTime, startVolume, endVol];
+		}
 	}
 
 	this.addLoopEvent = function(track) {
+		var check = checkListFor(LOOP, track);
+		var endTime = (track.getDuration() - track.getTime()) * 1000 + now;
 
+		if (check == "none") {
+			eventList.push([LOOP, track, endTime]);
+		} else {
+			eventList[check] = [LOOP, track, endTime];
+		}
 	}
 
 	function runList(){
+		for (var i in eventList) {
+			if (eventList[i][0] == FADE) {
+
+			}
+			if (eventList[i][0] == LOOP) {
+				if (!eventList[i][1].getPaused()) {
+					if (eventList[i][2] < now) {
+						eventList[i][1].triggerLoop();
+						eventList[i][2] = eventList[i][1].getDuration() * 1000 + now;
+					}
+				} else {eventList[i] = [REMOVE];}
+			}
+		}
 
 	}
 
 	function cleanupList() {
-
+		eventList.sort(function(a, b){return b-a};
+		while (eventList[eventList.length - 1] == REMOVE) {
+			eventList.pop();
+		}
 	}
 
-	function checkListFor(){
-
+	function checkListFor(eventType, track){
+		for (var i in eventList) {
+			if (eventList[i][0] == eventType) {
+				if (eventList[i][1] == track) {
+					return i;
+				}
+			}
+		}
+		return "none";
 	}
 }
