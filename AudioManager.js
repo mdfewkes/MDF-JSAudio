4bde60cb3e308bd8c8bb68dea68e106625f8b390
M D Fewkes <mdfewkes@gmail.com>
2018-02-10 23:55:00 -0700

    Setup index for better demonstration

--- AudioManager.js
+++ AudioManager.js
@@ -4,12 +4,14 @@ isMuted = false;
 //set sound clips and music tracks here
 var track1 = new musicTrackLoopingWTail("Audio/lay1tail8", 8);
 var track2 = new musicTrackLoopingWTail("Audio/lay2tail8", 8);
+var track3 = new musicTrackLoopingWTail("Audio/lay3tail8", 8);
 var backgroundMusic = new musicContainer(track1);
 
 //var clip1 = new sfxClipOverlap("Audio/Death", 3);
 
 track1.setTrackName("track1");
 track2.setTrackName("track2");
+track3.setTrackName("track3");
 
 
 
